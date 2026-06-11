fe1d2e57e098d94c1e4fc85098d5598c1f0f47dc
Nikolay <1inchunikhin@gmail.com>
2026-06-11 22:10:49 +0300

    chore: add focused logger diagnostics

--- TennisTournamentPackage/Sources/TennisTournamentFeature/Core/DependencyContainer/AppDependencyContainer.swift
+++ TennisTournamentPackage/Sources/TennisTournamentFeature/Core/DependencyContainer/AppDependencyContainer.swift
@@ -1,17 +1,29 @@
 import Foundation
+import LoGGer
 
 public struct AppDependencyContainer {
     public let tournamentRepository: any TournamentRepository
     public let standingsCalculator: any StandingsCalculating
     public let sessionStore: any AppSessionStoring
+    public let logger: Logger
+
+    public static func makeDefaultLogger() -> Logger {
+        Logger {
+            ConsoleDestination()
+                .withFormatter(PrettyFormatter(components: .full))
+                .withFilter(LevelFilter(.debug))
+        }
+    }
 
     public init(
         tournamentRepository: any TournamentRepository = InMemoryTournamentRepository(),
         standingsCalculator: any StandingsCalculating = ClassicStandingsCalculator(),
-        sessionStore: any AppSessionStoring = UserDefaultsAppSessionStore()
+        sessionStore: any AppSessionStoring = UserDefaultsAppSessionStore(),
+        logger: Logger? = nil
     ) {
         self.tournamentRepository = tournamentRepository
         self.standingsCalculator = standingsCalculator
         self.sessionStore = sessionStore
+        self.logger = logger ?? Self.makeDefaultLogger()
     }
 }
